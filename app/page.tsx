6507610c4df01fc0fb3ad044bf1d0b5b76dd7a7c
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2024-10-04 22:19:33 +0200

    add bento grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import { Banner } from "./Components/Banner";
+import Grid from "./Components/Grid";
 import Navbar from "./Components/Navbar";
 
 export default function Home() {
   return (
-    <div className="h-full w-full relative">
+    <main className="h-full w-full ">
       <Banner />
-      <div className="fixed bottom-0 w-full opacity-90">
-        <Navbar />
-      </div>
-    </div>
+      <Navbar />
+      <Grid />
+    </main>
   );
 }
