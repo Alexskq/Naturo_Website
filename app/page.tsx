e903ccb75953e018ce74907235a0a97b7bda26c5
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2024-10-06 15:54:12 +0200

    fix banner responsive/ add menu responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,45 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { Banner } from "./Components/Banner";
+import { Menu } from "./Components/Menu";
 import Navbar from "./Components/Navbar";
 
 export default function Home() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 400);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className="h-full w-full relative">
+    <div className="flex flex-col h-full w-full">
       <Banner />
-      <div className="fixed bottom-0 w-full opacity-90">
+      {/* <div
+        className={`fixed w-full opacity-90 ${
+          isMobile ? "navbar-top" : "bottom-0"
+        }`}
+      >
         <Navbar />
-      </div>
+      </div> */}
+      {isMobile ? (
+        <div className="fixed top-0 w-full opacity-90 z-50">
+          <Navbar />
+        </div>
+      ) : (
+        <div className="w-full opacity-90 z-50">
+          <Menu />
+        </div>
+      )}
     </div>
   );
 }
