67e8c3ba509284a7350630d91178e3c696296acd
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2024-10-08 21:34:13 +0200

    add new contact design

--- app/Components/ContactMe.tsx
+++ app/Components/ContactMe.tsx
@@ -1,9 +1,9 @@
 export const ContactMe = () => {
   return (
-    <div className="min-h-screen bg-white py-6 flex flex-col justify-center sm:py-12">
+    <div className="min-h-full bg-white p-0 flex flex-col justify-center sm:py-12">
       <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-        <div className="absolute inset-0 bg-gradient-to-r from-indigo-700 to-purple-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-        <div className="text-white relative px-4 py-10 bg-indigo-400 shadow-lg sm:rounded-3xl sm:p-20">
+        <div className="absolute inset-0 bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-amber-200 via-green-200 to-emerald-300 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div className="text-white relative px-4 py-10 bg-[conic-gradient(at_bottom_left,_var(--tw-gradient-stops))] from-amber-300 via-green-200 to-amber-700 shadow-lg sm:rounded-3xl sm:p-20">
           <div className="text-center pb-6">
             <h1 className="text-3xl">Contact Us!</h1>
 
@@ -35,7 +35,7 @@ export const ContactMe = () => {
             />
 
             <textarea
-              className=" h-[121px] shadow mb-4 min-h-0 appearance-none border rounded h-64 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              className=" h-64 shadow mb-4 min-h-0 appearance-none border rounded h-64 w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               type="text"
               placeholder="Type your message here..."
               name="message"
--- app/page.tsx
+++ app/page.tsx
@@ -52,6 +52,7 @@ export default function Home() {
       <Divider />
       {/* <Contact />
        */}
+
       <ContactMe />
     </div>
   );
