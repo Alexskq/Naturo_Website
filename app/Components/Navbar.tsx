068afcd493b3f3fd71c0d38494671f51058eb599
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2024-10-21 09:58:46 +0200

    fix error navbar

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -21,7 +21,7 @@ const navigation = [
   { name: "Contact", href: "#", current: false },
 ];
 
-function classNames(...classes) {
+function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
