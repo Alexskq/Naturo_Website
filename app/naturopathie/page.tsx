e07d7594a8448f350ef0119fd8231de0a919c311
Zoonekynd Alexandre <alexzoonekynd@gmail.com>
2024-10-23 20:16:03 +0200

    design article page

--- app/naturopathie/page.tsx
+++ app/naturopathie/page.tsx
@@ -1,62 +1,70 @@
-import { House } from "lucide-react";
-import Link from "next/link";
+import { Menu } from "../Components/Menu";
 
 const Naturopathie = () => {
   return (
-    <div className="flex flex-col h-screen w-[80%] justify-start items-center mx-auto">
-      <div className="flex flex-col">
-        <p className="text-xl mt-10 font-bold text-[#979F77] self-start ml-5 flex justify-center items-center gap-2">
+    <div className="flex flex-col h-screen w-full justify-start items-center mx-auto">
+      <Menu />
+      <div className="flex flex-col mt-10">
+        {/* <p className="text-lg my-6 font-bold text-[#979F77] self-start flex justify-center items-center gap-2">
           <House size={20} strokeWidth={2} />
           <Link href="/">Accueil</Link>
-        </p>
-        <div className="artboard artboard-horizontal phone-1 bg-slate-500 my-4 h-72 relative mt-10">
-          812×375
-          <div className="bg-green-500 h-[200px] w-[80%] relative mx-auto top-[250px] opacity-50 flex justify-center items-center">
-            <h1 className="text-red-500 text-4xl font-bold">La Naturopathie</h1>
-          </div>
+        </p> */}
+        {/* <div className="artboard artboard-horizontal phone-1 bg-slate-500 my-4 h-72 relative mt-10"> */}
+
+        <div
+          className=" bg-[url('/oil.jpg')] bg-no-repeat bg-cover bg-center  w-[95vw] h-[200px]"
+          style={{
+            backgroundImage:
+              "linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url('/oil.jpg')",
+          }}
+        ></div>
+        <div className="bg-green-500 h-[100px] w-[80%] relative top-[-50px] mx-auto opacity-50 flex justify-center items-center">
+          <h1 className="text-red-500 text-4xl font-bold">La Naturopathie</h1>
         </div>
       </div>
-      <div className="flex flex-col items-center mt-32 pb-40">
+      <div className="flex flex-col items-center pb-40">
         <h2 className="text-lg font-semibold mb-3"> Titre de l&apos;article</h2>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
-          incidunt similique itaque esse aliquam architecto! Atque, deserunt
-          quis perspiciatis error aliquam, facilis reprehenderit officia
-          voluptatum tempore ipsam voluptas deleniti? Excepturi. Lorem ipsum
-          dolor sit amet, consectetur adipisicing elit. Fuga, sunt pariatur!
-          Fugit possimus debitis voluptate aspernatur adipisci. Tenetur nostrum
-          ut velit libero in cumque sequi, dolorem omnis, doloremque eos
-          laboriosam! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          Voluptas omnis similique, mollitia nostrum adipisci laudantium
-          laboriosam ad amet doloribus repudiandae ullam nam doloremque ipsum
-          blanditiis, dolores, unde quo? In, veritatis.
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
-          incidunt similique itaque esse aliquam architecto! Atque, deserunt
-          quis perspiciatis error aliquam, facilis reprehenderit officia
-          voluptatum tempore ipsam voluptas deleniti? Excepturi. Lorem ipsum
-          dolor sit amet, consectetur adipisicing elit. Fuga, sunt pariatur!
-          Fugit possimus debitis voluptate aspernatur adipisci. Tenetur nostrum
-          ut velit libero in cumque sequi, dolorem omnis, doloremque eos
-          laboriosam! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          Voluptas omnis similique, mollitia nostrum adipisci laudantium
-          laboriosam ad amet doloribus repudiandae ullam nam doloremque ipsum
-          blanditiis, dolores, unde quo? In, veritatis.
-        </p>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
-          incidunt similique itaque esse aliquam architecto! Atque, deserunt
-          quis perspiciatis error aliquam, facilis reprehenderit officia
-          voluptatum tempore ipsam voluptas deleniti? Excepturi. Lorem ipsum
-          dolor sit amet, consectetur adipisicing elit. Fuga, sunt pariatur!
-          Fugit possimus debitis voluptate aspernatur adipisci. Tenetur nostrum
-          ut velit libero in cumque sequi, dolorem omnis, doloremque eos
-          laboriosam! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-          Voluptas omnis similique, mollitia nostrum adipisci laudantium
-          laboriosam ad amet doloribus repudiandae ullam nam doloremque ipsum
-          blanditiis, dolores, unde quo? In, veritatis.
-        </p>
+        <div className="flex flex-col justify-center items-center mx-5 text-justify md:mx-20">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
+            incidunt similique itaque esse aliquam architecto! Atque, deserunt
+            quis perspiciatis error aliquam, facilis reprehenderit officia
+            voluptatum tempore ipsam voluptas deleniti? Excepturi. Lorem ipsum
+            dolor sit amet, consectetur adipisicing elit. Fuga, sunt pariatur!
+            Fugit possimus debitis voluptate aspernatur adipisci. Tenetur
+            nostrum ut velit libero in cumque sequi, dolorem omnis, doloremque
+            eos laboriosam! Lorem ipsum dolor, sit amet consectetur adipisicing
+            elit. Voluptas omnis similique, mollitia nostrum adipisci laudantium
+            laboriosam ad amet doloribus repudiandae ullam nam doloremque ipsum
+            blanditiis, dolores, unde quo? In, veritatis.
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
+            incidunt similique itaque esse aliquam architecto! Atque, deserunt
+            quis perspiciatis error aliquam, facilis reprehenderit officia
+            voluptatum tempore ipsam voluptas deleniti? Excepturi. Lorem ipsum
+            dolor sit amet, consectetur adipisicing elit. Fuga, sunt pariatur!
+            Fugit possimus debitis voluptate aspernatur adipisci. Tenetur
+            nostrum ut velit libero in cumque sequi, dolorem omnis, doloremque
+            eos laboriosam! Lorem ipsum dolor, sit amet consectetur adipisicing
+            elit. Voluptas omnis similique, mollitia nostrum adipisci laudantium
+            laboriosam ad amet doloribus repudiandae ullam nam doloremque ipsum
+            blanditiis, dolores, unde quo? In, veritatis.
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor
+            incidunt similique itaque esse aliquam architecto! Atque, deserunt
+            quis perspiciatis error aliquam, facilis reprehenderit officia
+            voluptatum tempore ipsam voluptas deleniti? Excepturi. Lorem ipsum
+            dolor sit amet, consectetur adipisicing elit. Fuga, sunt pariatur!
+            Fugit possimus debitis voluptate aspernatur adipisci. Tenetur
+            nostrum ut velit libero in cumque sequi, dolorem omnis, doloremque
+            eos laboriosam! Lorem ipsum dolor, sit amet consectetur adipisicing
+            elit. Voluptas omnis similique, mollitia nostrum adipisci laudantium
+            laboriosam ad amet doloribus repudiandae ullam nam doloremque ipsum
+            blanditiis, dolores, unde quo? In, veritatis.
+          </p>
+        </div>
       </div>
 
       {/* <p>Contenu de la page La Naturopathie...</p> */}
